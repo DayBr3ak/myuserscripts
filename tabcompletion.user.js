0f83743e15587b8efaab2c4bc5b31916ba739662
ben <benj.groeneveld@gmail.com>
2017-12-05 14:59:25 +0100

    test webhook sync

--- tabcompletion.user.js
+++ tabcompletion.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Twitch Tab Completion (compatible with BTTV)
 // @namespace    https://openuserjs.org/users/daybreakz
-// @version      1.0.6
+// @version      1.0.7
 // @description  Tab completion for emotes (and users) also add chat history. BetterTTV
 // @author       Daybr3akz
 // @license      MIT
