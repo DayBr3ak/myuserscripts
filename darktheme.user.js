7f5d49e98c8fca6d6676745b9dd82fc505cf05f2
ben <benj.groeneveld@gmail.com>
2017-12-05 22:30:50 +0100

    documentstart

--- darktheme.user.js
+++ darktheme.user.js
@@ -1,18 +1,21 @@
 // ==UserScript==
+// @run-at       document-start
 // @name         Twitch BetterTTV without extension and better dark theme
 // @namespace    http://tampermonkey.net/
-// @version      1.0.9
+// @version      1.0.10
 // @description  This script load bttv without having to download the extension; Also have a better dark theme, try it!
 // @author       Daybr3akz
 // @license      MIT
 // @copyright    2017, daybreakz (https://openuserjs.org/users/daybreakz)
 // @match        https://www.twitch.tv/*
-// @grant        none
 // ==/UserScript==
 
 // // ==OpenUserJS==
 // @author daybreakz
 // ==/OpenUserJS==
+
+const BTTV_URL = 'https://cdn.betterttv.net/betterttv.js';
+const DEFAULT_DARK_THEME = 'https://userstyles.org/styles/userjs/148766/dark-theme-for-twitchtv.user.js';
 
 /*
  * This user script loads 2 external scripts aka
@@ -20,28 +23,30 @@
  * https://userstyles.org/styles/userjs/148766/dark-theme-for-twitchtv , see https://usertyles.org/
  */
 
-// fix screen glitch when player goes from theatre mode to full screen.
-const css = [ '<style>',
-    '.video-player--theatre.video-player--fullscreen .video-player__container {',
-    '    bottom: 0rem!important',
-    '}</style>'].join('\n');
+(function patchCss() {
+    // fix screen glitch when player goes from theatre mode to full screen.
+    const css = `
+    .video-player--theatre.video-player--fullscreen .video-player__container {
+        bottom: 0rem!important
+    }`;
+    const style = document.createElement('style');
+    style.textContent = css;
+    document.documentElement.appendChild(style);
+})();
 
 (function betterttv() {
     const script = document.createElement('script');
     script.type = 'text/javascript';
-    script.src = 'https://cdn.betterttv.net/betterttv.js';
+    script.src = BTTV_URL;
+    // script.src = 'https://localhost/betterttv.js';
     script.onload = () => {
         console.log('BetterTTV loaded');
-        document.head.appendChild($(css)[0]);
     };
-    const head = document.getElementsByTagName('head')[0];
-    if (!head) return;
-    head.appendChild(script);
+    document.documentElement.appendChild(script);
 })();
 
-const DEFAULT_DARK_THEME = 'https://userstyles.org/styles/userjs/148766/dark-theme-for-twitchtv.user.js';
 (function darkThemeManager() {
-    function loadStyle(src, callback) {
+    async function loadStyle(src, callback) {
         // nothing malicious, this is a Stylish userscript loading some css;
         // we are going to hook that css, allowing to disable it in light mode
 
@@ -61,9 +66,7 @@ const DEFAULT_DARK_THEME = 'https://userstyles.org/styles/userjs/148766/dark-the
         const script = document.createElement('script');
         script.type = 'text/javascript';
         script.src = src;
-        const head = document.getElementsByTagName('head')[0];
-        if (!head) return;
-        head.appendChild(script);
+        document.documentElement.appendChild(script);
     }
 
     let styleNode;
@@ -77,7 +80,7 @@ const DEFAULT_DARK_THEME = 'https://userstyles.org/styles/userjs/148766/dark-the
         // setup an observer to detect body class changes
         let bodyHasDark = 'unknown';
         bodyObserver = new MutationObserver(() => {
-            const current = $('body').hasClass('theme--dark');
+            const current = document.body.classList.contains('theme--dark');
             if (bodyHasDark !== current) {
                 bodyHasDark = current;
                 onThemeChange(current);
@@ -93,8 +96,8 @@ const DEFAULT_DARK_THEME = 'https://userstyles.org/styles/userjs/148766/dark-the
 
     loadStyle(DEFAULT_DARK_THEME, loadedStyleNode => {
         styleNode = loadedStyleNode;
-        onThemeChange($('body').hasClass('theme--dark'));
+        onThemeChange(document.body.classList.contains('theme--dark'));
     });
 
-    setupObserver();
+    setTimeout(setupObserver, 0);
 })();
