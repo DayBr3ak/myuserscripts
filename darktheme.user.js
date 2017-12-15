6753623fe8b3a13ea9b6d6d7df4ed65eca43f9c1
Benjamin G <benj.groeneveld@gmail.com>
2017-12-15 14:54:48 +0100

    specify chrome flag

--- darktheme.user.js
+++ darktheme.user.js
@@ -2,7 +2,7 @@
 // @run-at       document-start
 // @name         Twitch BetterTTV without extension
 // @namespace    http://tampermonkey.net/
-// @version      1.1.1
+// @version      1.1.2
 // @description  This script load bttv without having to download the extension;
 // @author       Daybr3akz
 // @license      MIT
@@ -14,7 +14,15 @@
 // // ==OpenUserJS==
 // @author daybreakz
 // ==/OpenUserJS==
+
+/*
+ * This user script loads 1 external scripts aka
+ * https://cdn.betterttv.net/betterttv.js, see https://github.com/night/BetterTTV
+ */
+
 const STORAGE_ENTRY = 'bttv_from_localhost';
+const LOCALHOST_WARNING = `Add ' chrome://flags/#allow-insecure-localhost ' to allow loading from localhost;
+Don't forget to change the CDN url in "src/utils/cdn.js" to localhost, otherwise styles won't reflect your changes.`;
 
 let BTTV_URL = 'https://cdn.betterttv.net/betterttv.js';
 const fromLocalhost = localStorage.getItem(STORAGE_ENTRY) === 'true';
@@ -26,13 +34,9 @@ unsafeWindow.toggleBttvDev = () => {
     localStorage.setItem(STORAGE_ENTRY, !fromLocalhost);
     console.log(`changed bttv script to ${fromLocalhost ? 'cdn.betterttv.net' : 'localhost'}. You can refresh to see the changes.`);
     if (!fromLocalhost) {
-        console.warn('Don\'t forget to change the CDN url in "src/utils/cdn.js" to localhost, otherwise styles won\'t reflect your changes.');
+        console.warn(LOCALHOST_WARNING);
     }
 };
-/*
- * This user script loads 1 external scripts aka
- * https://cdn.betterttv.net/betterttv.js ,see https://github.com/night/BetterTTV
- */
 
 (function patchCss() {
     // fix screen glitch when player goes from theatre mode to full screen.
@@ -48,12 +52,12 @@ unsafeWindow.toggleBttvDev = () => {
 (function betterttv() {
     const script = document.createElement('script');
     script.type = 'text/javascript';
+    if (fromLocalhost) {
+        console.warn(LOCALHOST_WARNING);
+    }
     script.src = BTTV_URL;
-    // script.src = 'https://localhost/betterttv.js';
     script.onload = () => {
         console.warn(`BetterTTV loaded from ${fromLocalhost ? 'localhost' : 'cdn.betterttv.net'}`);
-        if (fromLocalhost)
-          console.warn('Don\'t forget to change the CDN url in "src/utils/cdn.js" to localhost, otherwise styles won\'t reflect your changes.');
     };
     document.documentElement.appendChild(script);
 })();
