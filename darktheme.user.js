c7134a9f9f368f7c77ded6d856a35419d7d255db
Benjamin G <benj.groeneveld@gmail.com>
2017-12-15 14:40:20 +0100

    no more touching the host file for dev

--- darktheme.user.js
+++ darktheme.user.js
@@ -2,20 +2,33 @@
 // @run-at       document-start
 // @name         Twitch BetterTTV without extension
 // @namespace    http://tampermonkey.net/
-// @version      1.1.0
+// @version      1.1.1
 // @description  This script load bttv without having to download the extension;
 // @author       Daybr3akz
 // @license      MIT
 // @copyright    2017, daybreakz (https://openuserjs.org/users/daybreakz)
 // @match        https://www.twitch.tv/*
+// @grant        unsafeWindow
 // ==/UserScript==
 
 // // ==OpenUserJS==
 // @author daybreakz
 // ==/OpenUserJS==
+const STORAGE_ENTRY = 'bttv_from_localhost';
 
-const BTTV_URL = 'https://cdn.betterttv.net/betterttv.js';
-
+let BTTV_URL = 'https://cdn.betterttv.net/betterttv.js';
+const fromLocalhost = localStorage.getItem(STORAGE_ENTRY) === 'true';
+if (fromLocalhost) {
+    BTTV_URL = 'https://localhost/betterttv.js';
+}
+unsafeWindow.toggleBttvDev = () => {
+    const fromLocalhost = localStorage.getItem(STORAGE_ENTRY) === 'true';
+    localStorage.setItem(STORAGE_ENTRY, !fromLocalhost);
+    console.log(`changed bttv script to ${fromLocalhost ? 'cdn.betterttv.net' : 'localhost'}. You can refresh to see the changes.`);
+    if (!fromLocalhost) {
+        console.warn('Don\'t forget to change the CDN url in "src/utils/cdn.js" to localhost, otherwise styles won\'t reflect your changes.');
+    }
+};
 /*
  * This user script loads 1 external scripts aka
  * https://cdn.betterttv.net/betterttv.js ,see https://github.com/night/BetterTTV
@@ -38,7 +51,9 @@ const BTTV_URL = 'https://cdn.betterttv.net/betterttv.js';
     script.src = BTTV_URL;
     // script.src = 'https://localhost/betterttv.js';
     script.onload = () => {
-        console.log('BetterTTV loaded');
+        console.warn(`BetterTTV loaded from ${fromLocalhost ? 'localhost' : 'cdn.betterttv.net'}`);
+        if (fromLocalhost)
+          console.warn('Don\'t forget to change the CDN url in "src/utils/cdn.js" to localhost, otherwise styles won\'t reflect your changes.');
     };
     document.documentElement.appendChild(script);
 })();
